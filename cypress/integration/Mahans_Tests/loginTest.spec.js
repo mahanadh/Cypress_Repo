86c2874db1dc0ebd3991457819181681b87e64bd
mahanadh <mahan2054@gmail.com>
2021-03-30 22:44:33 +0545

    First Push of required changes

---
+++ cypress/integration/Mahans_Tests/loginTest.spec.js
@@ -0,0 +1,12 @@
+describe('My First Test Suite', function() {
+    it('My First Test Case', function(){
+        // all test steps for this test case should come here
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/") // to go to a website
+
+    })
+
+    it('My Second Test Case', function(){
+
+    })
+
+})
